3224f9c59e27c1a16859f847d2a08c38383609cf
brooksph <philliptbrooks@gmail.com>
2017-02-08 15:50:35 -0800

    modified gitignore

---
+++ mircea_project/data/results/found_but_not_present_k51.sh
@@ -0,0 +1,4 @@
+sourmash sbt_search -k 51 --dna mircea_51 ../signatures/GCF_000008125.1_ASM812v1_genomic.fna.gz.sig --threshold=0.001
+sourmash sbt_search -k 51 --dna mircea_51 ../signatures/GCF_000025525.1_ASM2552v1_genomic.fna.gz.sig --threshold=0.001
+sourmash sbt_search -k 51 --dna mircea_51 ../signatures/GCF_001306135.1_ASM130613v1_genomic.fna.gz.sig --threshold=0.001
+
